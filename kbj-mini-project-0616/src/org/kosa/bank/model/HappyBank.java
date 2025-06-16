0eec0542136cdf53b437cc9306834bc652c05c81
beomjunkim-soul <kbg142337@naver.com>
2025-06-16 14:23:19 +0900

    feat : 입금 작업 수정

--- kbj-mini-project-0616/src/org/kosa/bank/model/HappyBank.java
+++ kbj-mini-project-0616/src/org/kosa/bank/model/HappyBank.java
@@ -3,6 +3,8 @@ package org.kosa.bank.model;
 public class HappyBank {
 	public void withdraw() {}
 	public void find() {}
-	public void deposit(){}
+	public void deposit(){
+		System.out.println("sjw입금 수정");
+	}
 	public void delete() {}
 }
